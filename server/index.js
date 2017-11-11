570277fee11556e9a14a12da94be2e37b65b0c77
Phoebe <meipbe@gmail.com>
2017-11-10 20:12:22 -0800

    switched environment variables

--- server/index.js
+++ server/index.js
@@ -4,4 +4,9 @@ const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-// app.get('/', (req, res) => res.send('Hello World!'));
+// app.get('/', (req, res) => send image from database);
+
+// app.post()
+//   save received data to database
+
+// getPhotoFromUnsplash
