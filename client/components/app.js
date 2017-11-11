76a0f6ec015ee48ea8bca6a2af9063ad528da74a
Phoebe <meipbe@gmail.com>
2017-11-10 23:34:31 -0800

    Package updates

--- client/components/app.js
+++ client/components/app.js
@@ -1,10 +1,21 @@
 angular.module('pic-em', [])
   .component('app', {
-    controller: function() {
+    controller: function(connectToServer) {
+
+      this.picOfTheDay = ''
+      this.userCollection = [];
+
+      this.getPhotos = function() {
+        connectToServer.getPhotos()
+        .then(photo => console.log(JSON.parse(photo)))
+        .catch(err => console.error(err));
+      }
+
     },
 
     template: `
       <div>
+        <button ng-click="$ctrl.getPhotos()">derp</button>
         <photo-display></photo-display>
         <photo-collection><photo-collection/>
       </div>
