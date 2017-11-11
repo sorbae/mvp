be5d43f57dca545841b032492b559e2b0e39285b
Phoebe <meipbe@gmail.com>
2017-11-10 23:34:17 -0800

    Added headers

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const unsplash = require('./getRandomPhoto.js');
 const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`listening on port ${port}`));
 
-// app.get('/', (req, res) => send image from database);
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
+
+app.get('/hello', (req, res) => {
+  unsplash.getUnsplashPhoto()
+  .then((photo) => res.json(photo))
+  .catch(err => console.error(err))
+});
 
 // app.post()
 //   save received data to database
