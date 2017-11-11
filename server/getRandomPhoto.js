1959fc4f7f9025fc6f610e3eb4b37809b027310b
Phoebe <meipbe@gmail.com>
2017-11-10 23:33:52 -0800

    Removed comments

--- server/getRandomPhoto.js
+++ server/getRandomPhoto.js
@@ -1,20 +1,13 @@
 const rp = require('request-promise');
 const config = require('../config.js');
 
-let getUnsplashPhoto = function(query) {
+module.exports.getUnsplashPhoto = function() {
   let unsplash = {
     url: 'https://api.unsplash.com/photos/random',
     headers: {
       Authorization: `Client-ID ${config.CONFIG.AUTHORIZATION}`,
-    },
-    query: query
+    }
   };
 
   return rp(unsplash)
-    // .then(function(photo) {
-    //   console.log(photo);
-    // })
-    // .catch(function(err) {
-    //   console.log('Get error occured: ', err);
-    // })
 }
