277c4ea940fc4158868bbf7339dba57f750e6c4f
Phoebe <meipbe@gmail.com>
2017-11-11 16:31:38 -0800

    Successfully saves and fetches entries

--- database/db.js
+++ database/db.js
@@ -1,14 +1,13 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/index.js');
+mongoose.connect('mongodb://localhost/collection');
 
 mongoose.connection.on('error', console.error.bind(console, 'connection error'));
 mongoose.connection.once('open', function() { console.log('Database connected!') })
 
 const photoEntry = mongoose.Schema({
-  id: String,
+  id: {type: String, unique: true, index: true},
   photographer: String,
   description: String,
-  location: String,
   url: String,
   thumbnail: String,
   portfolio: String
@@ -16,18 +15,23 @@ const photoEntry = mongoose.Schema({
 
 let Photo = mongoose.model('Photo', photoEntry);
 
-let createNewPhotoEntry = function(photo) {
+module.exports.createNewPhotoEntry = function(photo) {
   let photoDetails = {
     id: photo.id,
     photographer: photo.user.name,
     description: photo.description,
-    location: `${photo.location.city}, ${photo.location.country}`,
     url: photo.urls.full,
     thumbnail: photo.urls.thumb,
     portfolio: photo.user.portfolio_url
   }
 
-  new Photo(photoDetails).save(function(err) {
-    if (err) return console.error(err);
-  });
+  return new Photo(photoDetails).save(function(err) {
+    if (err) return console.error('error saving new photo entry: ', err)
+  })
+}
+
+module.exports.fetchUserCollection = function() {
+  return Photo.find(function(err, photos) {
+    if (err) return console.error('error retrieving user collection: ', err)
+  })
 }
